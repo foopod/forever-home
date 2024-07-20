af06051ecbf635dd162874bf336e7658ca63ae0e
Jono Shields <jonathonshields@gmail.com>
2024-07-20 15:01:18 +1200

    Add call to trade api

--- src/components/Trade.tsx
+++ src/components/Trade.tsx
@@ -1,7 +1,9 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react"
+import { Dispatch, SetStateAction, useContext, useEffect, useState } from "react"
 import { FaTimes } from "react-icons/fa"
 import QRCode from "react-qr-code"
 import Scanner from "./Scanner"
+import { Audio } from "react-loader-spinner"
+import { GameContext } from "../context/GameContext"
 
 interface Props {
     userID: string
@@ -12,9 +14,25 @@ interface Props {
 const Trade: React.FC<Props> = ({userID, isOpen, setIsOpen}) => {
     const [scannerOpen, setScannerOpen] = useState(false)
     const [scanResult, setScanResult] = useState('')
+    const [loading, setLoading] = useState(false)
+    const { currentState, setCurrentState } = useContext(GameContext)
 
     const toggleScan = () => {
       setScannerOpen(prev => !prev)
+    }
+
+    const handleScan = async (result: string) => {
+        // result should be a userID
+        setLoading(true)
+
+        // call trade api
+        const response = await fetch(`http://localhost:8080/api/swap/${currentState.player.id}/${result}`)
+        const json = await response.json()
+        setCurrentState(json)
+
+        setLoading(false)
+        // At the end close scanner
+        setScannerOpen(false)
     }
     
     useEffect(() => {
@@ -29,23 +47,36 @@ const Trade: React.FC<Props> = ({userID, isOpen, setIsOpen}) => {
         <> {
             isOpen &&
                 <div className="w-full h-full bg-white absolute top-0 left-0 flex flex-col justify-center">
-                    <p>{scanResult}</p>
-                    
-                    <div className="m-5">
-                        <QRCode
-                            size={256}
-                            className="max-md:h-max max-md:w-full"
-                            value={userID}
-                            viewBox={`0 0 256 256`}
+                    { !loading &&
+                        <>
+                            <div className="m-5">
+                                <QRCode
+                                    size={256}
+                                    className="max-md:h-max max-md:w-full"
+                                    value={userID}
+                                    viewBox={`0 0 256 256`}
+                                />
+                            </div>
+                            <button onClick={toggleScan} className='bg-slate-200 px-4 py-2 rounded-md mx-2'>Scan a QR</button>
+                            <div className='absolute top-0 right-0' onClick={() =>  {setIsOpen(false)}}>
+                                <FaTimes color='black' size={"4em"}/>
+                            </div>
+                        </>
+                    }
+                    { loading &&
+                        <Audio
+                            height="100"
+                            width="100"
+                            color="#4fa94d"
+                            ariaLabel="audio-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="wrapper-class"
+                            visible={true}
                         />
-                    </div>
-                    <button onClick={toggleScan} className='bg-slate-200 px-4 py-2 rounded-md mx-2'>Scan a QR</button>
-                    <div className='absolute top-0 right-0' onClick={() =>  {setIsOpen(false)}}>
-                        <FaTimes color='black' size={"4em"}/>
-                    </div>
+                    }
                     {scannerOpen &&
                         <>
-                            <Scanner isActive={scannerOpen} setIsActive={setScannerOpen} setResult={setScanResult}/>
+                            <Scanner isActive={scannerOpen} setIsActive={setScannerOpen} handleScan={handleScan}/>
                         </>
                     }
                 </div>
