b683e9c27d60616623f417a306d0aaa56a0a4f05
Jono Shields <jonathonshields@gmail.com>
2024-07-21 11:22:18 +1200

    fix websocket env var

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 const API_ENDPOINT = import.meta.env.VITE_REACT_APP_API_ENDPOINT
 const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT
-const WS_ENDPOINT = import.meta.env.WS_ENDPOINT
+const WS_ENDPOINT = import.meta.env.VITE_REACT_APP_WS_ENDPOINT
 
 function isProduction() {
     return ENVIRONMENT === 'production'
