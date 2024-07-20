24403192d5a62b1a6edd4eb1cb499770c922c003
Jono Shields <jonathonshields@gmail.com>
2024-07-20 20:23:35 +1200

    update border

--- src/components/LayeredImage.tsx
+++ src/components/LayeredImage.tsx
@@ -9,9 +9,9 @@ const LayeredImage: React.FC<Props> = ({attributes}) => {
     return (
         <div className="grid grid-cols-1">
             <img className='col-start-1 row-start-1' src={`/parts/${attributes.species}/base/${attributes.base}.png`} />
-            <img className='col-start-1 row-start-1' src={`/parts/${attributes.species}/accessories/${attributes.accessory}.png`} />
             <img className='col-start-1 row-start-1' src={`/parts/${attributes.species}/eyes/${attributes.eyes}.png`} />
             <img className='col-start-1 row-start-1' src={`/parts/${attributes.species}/mouth/${attributes.mouth}.png`} />
+            <img className='col-start-1 row-start-1' src={`/parts/${attributes.species}/accessories/${attributes.accessory}.png`} />
         </div>
     )
 }
