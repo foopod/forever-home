94dfad8811e5d4466fe61a4dbb9039e897cce355
John Cave <john@johncave.co.nz>
2024-07-20 21:18:10 +1200

    Log env properly

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ const API_ENDPOINT = import.meta.env.VITE_REACT_APP_API_ENDPOINT
 const ENVIRONMENT = import.meta.env.ENVIRONMENT
 
 function isProduction() {
-    console.log("We are in production")
+    console.log("Environment:", ENVIRONMENT)
     return ENVIRONMENT === 'production'
 }
 
