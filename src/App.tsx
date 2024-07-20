66e13a4d8136993afec58ac4dbf1b9727794facf
Jono Shields <jonathonshields@gmail.com>
2024-07-20 12:13:03 +1200

    fix up styles

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <main>
       <Join />
-      <h1 className='text-3xl font-bold underline'>Forever Home</h1>
+      <h1 className='mb-4 text-4xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl'>Forever Home</h1>
       {scannerOpen &&
         <>
           <Scanner isActive={scannerOpen} setIsActive={setScannerOpen} setResult={setScanResult}/>
@@ -27,11 +27,21 @@ function App() {
         
       }
       {! scannerOpen &&
-        <>
-          <button onClick={toggleScan} className='bg-slate-200 px-4 py-2 rounded-md'>Scan</button>
+        <div className='flex flex-col items-center'>
+          <div className='m-3 flex space-around'>
+            <span className='bg-green-500 text-white px-4 py-2 rounded-md mx-2'>78% Match</span>
+            <button onClick={toggleScan} className='bg-slate-200 px-4 py-2 rounded-md mx-2'>Trade Pets</button>
+          </div>
+          <div className='m-2'>
+            <p>Margaret</p>
+            <img src="https://placehold.co/400"/>
+          </div>
+          <div className='m-2'>
+            <p>Bubbles</p>
+            <img src="https://placehold.co/400"/>
+          </div>
           <LayeredImage pet={cat_image} />
-          <p>Result: {scanResult}</p>
-        </>
+        </div>
       }
     </main>
   )
--- src/components/Join.tsx
+++ src/components/Join.tsx
@@ -10,7 +10,13 @@ const Join = () => {
                 <div className="w-full h-full bg-white absolute top-0 left-0 flex flex-col justify-center">
                     <p>Wanna help someone find their...</p>
                     <h1 className="mb-4 text-4xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">Forever Home?</h1>
-                    <div>
+                    <div className="prose max-w-sm text-left mx-auto">
+                        <ul>
+                            <li>Find the pet that best matches your client</li>
+                            <li>Help as many pets as you can find their forever home!</li>
+                        </ul>
+                    </div>
+                    <div className="my-5">
                         <button onClick={() => {setIsOpen(false)}} className="bg-slate-200 px-4 py-2 rounded-md">Get Started!</button>
                     </div>
                 </div>
